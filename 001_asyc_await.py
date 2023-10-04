ce0f4eb16a21559267357c7ff636fe061459a285
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2023-10-04 14:56:05 +0530

    Update 001_asyc_await.py

--- 001_asyc_await.py
+++ 001_asyc_await.py
@@ -1,32 +1,39 @@
 import asyncio
 
+
 async def xx():
-    # we cant use await with `print` as it is not a goroutine
-    # await print("xx")
     print("xx started")
-    await asyncio.sleep(0)  # Yield control back to the event loop (not necessary in this case)
-    print("ohhhoo....")
-    await yy()
+    await asyncio.sleep(1)  # Yield control back to the event loop (not necessary in this case)
+    print("xx says ohhhoo....")
     return "ok xx"
     
+
 async def yy():
     print("yy started")
-    await asyncio.sleep(0)  # Yield control back to the event loop (not necessary in this case)
+    await asyncio.sleep(2)  # Yield control back to the event loop (not necessary in this case)
+    print("yy says ohhhoo....")
     return "ok yy"
 
 
-
-# METHOD 1:
-ans1 = asyncio.run(xx())  # run the event loop and execute the asynchronous code.
-print(ans1)
-
-
-# METHOD 2 (BEST method):
-async def driver():
+# (BEST method) calling through :
+async def main_driver():
+    print("main_driver started ...")
     coroutines = [xx(), yy()]
     results = await asyncio.gather(*coroutines)
     print(results)
+    print("main_driver ended ...")
 
 
 event_loop = asyncio.get_event_loop()
-event_loop.run_until_complete(driver())
+event_loop.run_until_complete(main_driver())
+
+
+"""
+main_driver started ...
+xx started
+yy started
+xx says ohhhoo....
+yy says ohhhoo....
+['ok xx', 'ok yy']
+main_driver ended ...
+"""
