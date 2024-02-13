f870bca00b15d0083d11548d58729cf81b9a9e7b
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2024-02-13 09:59:49 +0530

    Update tut3_threading_to_big_function.py

--- tut3_threading_to_big_function.py
+++ tut3_threading_to_big_function.py
@@ -2,21 +2,17 @@
 Programme to run a big function task using MultiThreading
 """
 
+import sys
 import time
-
-import logging
-import random
 
 from threading import Thread
 from threading import Timer  # utilized to run a code after a specified time period
 
-# from concurrent.futures import Future, ThreadPoolExecutor, ProcessPoolExecutor
-# from concurrent.futures import as_completed
-# from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, Future
 
-from queue import Queue
-
-no_tasks = 7  #
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.INFO)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 def sub_task(name, x):
@@ -25,24 +21,24 @@ def sub_task(name, x):
     logging.info(f'Finishing Task {name}: {x}')
 
 
-sub_task_threads = []
-
-
 def long_task(name):
     """This Function will take 14 sec to complete"""
     # no_tasks = random.randrange(start=0, stop=10, step=1)
+    no_tasks = 10
     logging.info(f"Task: {name} performing {no_tasks} tasks with each 2 sec.")
+    sub_task_threads = []
     for x in range(no_tasks):
         t = Thread(target=sub_task, args=['thread: ' + str(x), x])
         sub_task_threads.append(t)
         t.start()
 
     logging.info(f'<<<< {name}: completed >>>>')
+    return sub_task_threads
 
 
 # Driver code .......................................
 logging.info('Starting')
-long_task("Main")
+sub_task_threads = long_task("Main")
 
 for t in sub_task_threads:
     t.join()
