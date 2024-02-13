5ffd9e7ffa23e78948fa52d88affa0f3c4a016d7
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2024-02-13 10:30:29 +0530

    Update tut3_threading_to_big_function.py

--- tut3_threading_to_big_function.py
+++ tut3_threading_to_big_function.py
@@ -16,32 +16,35 @@ logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 
 def sub_task(name, x):
-    logging.info(f'Starting Task {name}: {x}')
+    logger.info(f'Starting Task {name}: {x}')
     time.sleep(2)
-    logging.info(f'Finishing Task {name}: {x}')
+    logger.info(f'Finishing Task {name}: {x}')
 
 
 def long_task(name):
     """This Function will take 14 sec to complete"""
     # no_tasks = random.randrange(start=0, stop=10, step=1)
     no_tasks = 10
-    logging.info(f"Task: {name} performing {no_tasks} tasks with each 2 sec.")
+    logger.info(f"Task: {name} performing {no_tasks} tasks with each 2 sec.")
     sub_task_threads = []
     for x in range(no_tasks):
         t = Thread(target=sub_task, args=['thread: ' + str(x), x])
         sub_task_threads.append(t)
         t.start()
 
-    logging.info(f'<<<< {name}: completed >>>>')
+    logger.info(f'<<<< {name}: completed >>>>')
     return sub_task_threads
 
 
 # Driver code .......................................
-logging.info('Starting')
+logger.info('Starting')
 sub_task_threads = long_task("Main")
+
+tt = sub_task_threads[0]
+print(tt.is_alive(), tt.name, tt.isAlive())
 
 for t in sub_task_threads:
     t.join()
 
 
-logging.info(f'At the End of the File')
+logger.info(f'At the End of the File')
