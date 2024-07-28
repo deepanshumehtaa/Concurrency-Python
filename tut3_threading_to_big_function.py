4b05d05a1b9f99e5f560f92645d4187f2e6c5063
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2024-07-28 17:56:15 +0530

    Update tut3_threading_to_big_function.py

--- tut3_threading_to_big_function.py
+++ tut3_threading_to_big_function.py
@@ -10,6 +10,8 @@ from threading import Timer  # utilized to run a code after a specified time per
 
 
 import logging
+from typing import List
+
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 logger.addHandler(logging.StreamHandler(stream=sys.stdout))
@@ -17,34 +19,34 @@ logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
 def sub_task(name, x):
     logger.info(f'Starting Task {name}: {x}')
-    time.sleep(2)
+    time.sleep(5)
     logger.info(f'Finishing Task {name}: {x}')
 
 
-def long_task(name):
+def long_task(no_tasks) -> List[Thread]:
     """This Function will take 14 sec to complete"""
     # no_tasks = random.randrange(start=0, stop=10, step=1)
-    no_tasks = 10
-    logger.info(f"Task: {name} performing {no_tasks} tasks with each 2 sec.")
-    sub_task_threads = []
+    logger.info(f"long_task performing {no_tasks} tasks with each 5 sec.")
+    sub_task_threads: List[Thread] = []
     for x in range(no_tasks):
         t = Thread(target=sub_task, args=['thread: ' + str(x), x])
         sub_task_threads.append(t)
         t.start()
 
-    logger.info(f'<<<< {name}: completed >>>>')
+    logger.info('<<<< returning long_task >>>>')
     return sub_task_threads
 
 
 # Driver code .......................................
-logger.info('Starting')
-sub_task_threads = long_task("Main")
+logger.info('Starting ...')
 
-tt = sub_task_threads[0]
-print(tt.is_alive(), tt.name, tt.isAlive())
+no_tasks = 10
+sub_task_threads = long_task(no_tasks)
+tt: Thread = sub_task_threads[0]
+
+print(tt.is_alive(), tt.ident)
 
 for t in sub_task_threads:
     t.join()
 
-
-logger.info(f'At the End of the File')
+logger.info('Exit ...')
