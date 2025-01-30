0888177b0f2dbb5365f761fe271351642bf3684f
Deepanshu mehta <40016257+deepanshumehtaa@users.noreply.github.com>
2025-01-30 20:22:19 +0530

    Update async_await_asyncio_semaphore.py

--- async_await_asyncio_semaphore.py
+++ async_await_asyncio_semaphore.py
@@ -5,10 +5,27 @@ import threading
 semaphore = asyncio.Semaphore(3)  # Allow up to 3 concurrent accesses
 
 
+def some_blocking_fn():
+    time.sleep(1)
+
+global_dict = {}
+
+
 async def access_shared_resource(task_id):
     async with semaphore:
-        print(f"Task {task_id} is accessing the shared resource")
+        loop = asyncio.get_event_loop()
         await asyncio.sleep(2)  # Simulate some work
+
+        with threading.Lock():
+            global_dict[task_id] = task_id
+
+        print(f"Task {task_id} is accessing the shared resource")
+
+        # this is how we call blocking fn, in async way
+        res = await loop.run_in_executor(
+            executor=None,
+            func=some_blocking_fn,
+        )
 
 
 async def main():
@@ -19,6 +36,8 @@ async def main():
 
     await asyncio.gather(*tasks)
 
+
 start_time = time.time()
 asyncio.run(main())
 print(f"Total execution time: {time.time() - start_time} seconds")
+print(global_dict)
